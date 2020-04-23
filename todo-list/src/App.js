48a3befb61a155bc0e9f6d9d620c1ed54b5149ef
leducloi <dekaito193@gmail.com>
2020-04-23 19:41:38 +0700

    create TodoItem component

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TodoItem from './components/TodoItem';
 
 function App() {
   return (
@@ -10,6 +11,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
         <a
           className="App-link"
           href="https://reactjs.org"
