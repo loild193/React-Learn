580591556910a26c6608d2eb3b755d2e3a3c966c
leducloi <dekaito193@gmail.com>
2020-04-23 20:00:30 +0700

    create Title component

---
+++ sign-in/sign-in/src/components/Title.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'; 
+
+
+class Tittle extends Component {
+  render() {
+    return (
+      <div className="Tittle">
+        <h2>Sign In</h2>
+      </div>
+    );
+  }
+}
+
+export default Tittle;
