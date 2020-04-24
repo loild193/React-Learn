0e65cca652c80a07cec19f7821c931757aa8d6c2
leducloi <dekaito193@gmail.com>
2020-04-24 20:08:17 +0700

    using map for todoItems

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,30 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import TodoItem from './components/TodoItem';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <TodoItem title="Go to bed"/>
-        <TodoItem title="Play game"/>
-        <TodoItem title="Chat with gf"/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.todoItems = [
+      'Go to bed',
+      'Play game',
+      'Chat with gf'
+    ];
+  };
+  render() {
+    return (
+      <div className="App">
+        {
+          this.todoItems.map( (item, index) => <TodoItem key={index} title={item}/> )
+        }
+      </div>
+    );
+  } 
 }
 
 export default App;
